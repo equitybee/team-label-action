a1947147a1015a4c574360c5f834791330422e2d
Jonathan Levi <jonathan1996levi@gmail.com>
2021-11-05 11:26:31 +0000

    [actions/github] fix another import

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+import { context, getOctokit } from '@actions/github';
 
 async function run(): Promise<void> {
   try {
     // const token = core.getInput('repo-token', { required: true });
     // const configPath = core.getInput('configuration-path', { required: true });
 
-    const pullRequest = github.context.payload.pull_request;
+    const pullRequest = context.payload.pull_request;
     console.log(pullRequest);
     const author = pullRequest?.user.login;
     console.log(author);
@@ -18,7 +18,7 @@ async function run(): Promise<void> {
 
     const myToken = core.getInput('myToken');
 
-    const octokit = github.getOctokit(myToken);
+    const octokit = getOctokit(myToken);
 
     const org = 'equitybee';
     const teamList = await octokit.rest.teams.list({
