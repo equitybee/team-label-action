08d616e510119045727f93c72eff50d0c2830be9
Jonathan Levi <jonathan1996levi@gmail.com>
2021-11-05 10:57:18 +0000

    [WIP] Get PR info, author

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,34 @@
 import * as core from '@actions/core';
-import { wait } from './wait';
+import github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // const token = core.getInput('repo-token', { required: true });
+    // const configPath = core.getInput('configuration-path', { required: true });
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    const pullRequest = github.context.payload.pull_request;
+    console.log(pullRequest);
+    const author = pullRequest?.user.login;
+    console.log(author);
 
-    core.setOutput('time', new Date().toTimeString());
+    // get org -> provide id as input?
+    // if PR author is in the org
+    // get teams for PR author
+    // label PR with all team names for PR author
+
+    const myToken = core.getInput('myToken');
+
+    const octokit = github.getOctokit(myToken);
+
+    const org = 'equitybee';
+    const teamList = await octokit.rest.teams.list({
+      org,
+    });
+    console.log(teamList);
+    // await octokit.rest.teams.getMembershipForUserInOrg();
   } catch (error) {
     if (error instanceof Error) {
+      core.error(error);
       core.setFailed(error.message);
     }
   }
